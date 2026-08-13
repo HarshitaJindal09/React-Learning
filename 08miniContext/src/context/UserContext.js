728768ce6caf6b810941f701a90831ebbe33ac26
Harshita Jindal <harshitajindal162@gmail.com>
2026-08-13 20:18:32 +0530

    Context API basic

---
+++ 08miniContext/src/context/UserContext.js
@@ -0,0 +1,8 @@
+// every conetxt is a rapper and it raps all copoents and becone provider
+//and then all those copmenents can acess that conetxt and then we make a provider
+import React from 'react'
+
+const UserContext = React.createContext()
+
+
+export default UserContext;
